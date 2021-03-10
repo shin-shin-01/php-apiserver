db83555e53442f9b271678c9933cb871780b1023
Kaze Shindo <kaze.bknb@outlook.jp>
2021-03-10 14:11:14 +0900

    create HelloController

---
+++ src/Controller/Hellocontroller.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+class HelloController extends AppController
+{
+    public function world()
+    {
+        $this->set([
+            'message' => 'hello world',
+            '_serialize' => ['message'],
+        ]);
+    }
+}
